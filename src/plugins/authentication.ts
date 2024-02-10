1c3e497657c973483e79ee9f0ba9c09b1ff05f76
Ryan H <ryanzhutch@gmail.com>
2024-02-10 11:46:13 -0700

    Add new files and plugins

---
+++ src/plugins/authentication.ts
@@ -0,0 +1,23 @@
+import { FastifyInstance, FastifyRequest } from "fastify";
+import { fastifyPlugin } from "fastify-plugin";
+
+import { Unauthorized } from "@exceptions/error";
+
+export const authentication = fastifyPlugin(
+  (fastify: FastifyInstance, _: unknown, done: () => void) => {
+    const authPreHandler = async (request: FastifyRequest) => {
+      try {
+        const authorization =
+          (request.headers.authorization
+            ? request.headers.authorization?.split("Bearer ")[1]
+            : "") || "";
+
+        const payload = fastify.jwt.verify(authorization) as { email: string };
+      } catch (error) {
+        throw new Unauthorized();
+      }
+    };
+    fastify.decorate("authenticateUser", authPreHandler);
+    done();
+  }
+);
