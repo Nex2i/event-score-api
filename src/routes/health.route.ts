50bcf636647511b8072941fdf5a4a9dccb469441
Ryan H <ryanzhutch@gmail.com>
2024-02-10 22:12:09 -0700

    Add health route handler

---
+++ src/routes/health.route.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance, RouteOptions } from "fastify";
+
+export default async function Health(
+  fastify: FastifyInstance,
+  opts: RouteOptions
+) {
+  fastify.route({
+    method: "GET",
+    url: "/health",
+    handler: async (request, reply) => {
+      return { status: "ok" };
+    },
+  });
+}
