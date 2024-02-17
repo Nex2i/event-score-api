0d387ad5af2b08d739a1218496a7e7668fdf0e32
Ryan H <ryanzhutch@gmail.com>
2024-02-17 16:11:50 -0700

    Add database health check

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -1,3 +1,4 @@
+import { dbClient } from "@/db/db.client";
 import { FastifyInstance, RouteOptions } from "fastify";
 
 export default async function Health(
@@ -8,6 +9,7 @@ export default async function Health(
     method: "GET",
     url: "/health",
     handler: async (request, reply) => {
+      await dbClient.$queryRaw`SELECT 1`;
       return { status: "ok" };
     },
   });
